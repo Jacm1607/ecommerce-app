7fa49a5bc72f8aa821ab9944d8bced3851e9309e
Jose <chavarriaj84@gmail.com>
2023-09-16 11:32:49 -0400

    add component scrollCategoryHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import CarouselHome from "@/components/ui/page/carouselHome";
+import CategoryHome from "@/components/ui/page/categoryHome";
 
 export default function Home() {
   return (
-    <><CarouselHome /><main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <><CarouselHome />
+    <CategoryHome />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
     </main></>
   )
