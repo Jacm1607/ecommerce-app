479a662333e0fbf0fd216e92635b37b64a3bcd36
Jose <chavarriaj84@gmail.com>
2023-09-16 12:28:25 -0400

    config assets

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['www.dashboard.hauscenter.com.bo']
     },
+    assetPrefix: 'https://www.testing.hauscenter.com.bo',
 }
 
 module.exports = nextConfig
