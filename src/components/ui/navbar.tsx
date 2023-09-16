214b7d862b6f88b4473d0a48eec1964d8d714c0c
Jose <chavarriaj84@gmail.com>
2023-09-16 11:05:08 -0400

    element navbar y homepage

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -2,20 +2,22 @@ import Image from 'next/image'
 import { Button } from './button';
 import { Input } from './input';
 import Link from 'next/link';
+import { ItemsNavbar } from './itemsNavbar';
+import { CommandDemo } from './searchBuscador';
 
 const Navbar = () => {
     return (
-        <form action="" className="w-full h-full flex items-center" method="get">
+        <form action="" className="w-full h-full flex items-center" method="get" autoComplete='off' >
             <div className="h-[70px] bg-primary w-full sticky top-0 grid grid-cols-6 gap-5 z-10">
                 <div className="col-span-1 flex items-center ml-2">
                     <Link href={'/'}><Image src={'/logo-blanco-horizontal.png'} alt='Hauscenter' width={240} height={130} style={{ width: '70%', height: '70%' }}></Image></Link>
                 </div>
                 <div className="col-span-1 flex justify-end items-center">
-                    <Button type='button' variant={'secondary'}>Menu</Button>
+                    <ItemsNavbar />
                 </div>
                 <div className="col-span-2 flex w-full items-center">
                     <Input name='query' className=' rounded-r-none' type="text" placeholder="Buscar producto..." />
-                    {/* <Button className='rounded-l-none' variant={'secondary'} type="submit">Buscar</Button> */}
+                    <Button className='rounded-l-none' variant={'secondary'} type="submit">Buscar</Button>
                 </div>
                 <div className="col-span-2 flex justify-end items-center text-white space-x-2">
                     {/* <Link href={'/carrito-compra'} className='px-4 py-2'>Carrito</Link> */}
---
+++ src/components/ui/searchBuscador.tsx
@@ -0,0 +1,46 @@
+import {
+    Calculator,
+    Calendar,
+    CreditCard,
+    Settings,
+    Smile,
+    User,
+  } from "lucide-react"
+  
+  import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+  } from "@/components/ui/command"
+  
+  export function CommandDemo() {
+    return (
+      <Command className="rounded-lg border shadow-md">
+        <CommandInput placeholder="Type a command or search..." hidden={false} />
+        <CommandList className="hidden">
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions" >
+            <CommandItem>
+              <Calendar className="mr-2 h-4 w-4" />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <Smile className="mr-2 h-4 w-4" />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <Calculator className="mr-2 h-4 w-4" />
+              <span>Calculator</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+        </CommandList>
+      </Command>
+    )
+  }
+  
