b7ed2369509d81e98f40fd62cd08357b29e7b79b
Jose <chavarriaj84@gmail.com>
2023-09-16 11:45:08 -0400

    add component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import AdsHome from "@/components/ui/page/adsHome";
 import BranHome from "@/components/ui/page/brandHome";
 import CarouselHome from "@/components/ui/page/carouselHome";
 import CategoryHome from "@/components/ui/page/categoryHome";
+import ListProductsHome from "@/components/ui/page/listProductHome";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
     <CategoryHome />
     <BranHome />
     <AdsHome />
+    <ListProductsHome />
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
     </main></>
